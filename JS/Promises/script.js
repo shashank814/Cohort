57ab5909d9bee2fc156c6027ba49af8e565db11a
Shashank Singh <sshingh456@gmail.com>
2026-06-05 16:53:55 +0530

    Refactor promise handling and improve logging messages in script.js

--- JS/Promises/script.js
+++ JS/Promises/script.js
@@ -1,4 +1,4 @@
-// Promise -> 
+// Promise -> A Promise is an object that says: "I don't have the result right now, but I promise to give it to you when I do."
 
 async function data() {
     let response = await fetch('https://picsum.photos/v2/list')
@@ -23,32 +23,30 @@ let p1 = new Promise(function(resolve, reject) {
 })
 
 let p2 = new Promise(function(resolve, reject) {
-    console.log("Promise Pending Hai");
+    console.log("Promise Pending hai...");
     
-    let result = true
+    let result = true;
+
     setTimeout(function() {
         if(result) {
-            console.log('value true');
-
+            console.log("value true");
             resolve()
         } else {
             console.log("value false");
-
             reject()
         }
     }, 3000)
-
 })
 p2.then(function() {
-    console.log("Promise is fulfiiled"); 
+    console.log("Promise is fulfilled...");
 })
 .catch(function() {
-    console.log("Promise is rejected"); 
+    console.log("Promise is rejected...");
 })
-.finally(function() {
-    console.log("Promise END"); 
+.finally(function(){
+    console.log("Promise END");
+    
 })
-
 
 // Order food from zomato -> handling promises
 
@@ -60,39 +58,86 @@ function orderFood() {
 
         setTimeout(function() {
             if(orderStatus) {
-                console.log("Delivery wale bhaiya aa gye hai");
+                console.log("Delivery Done");
                 resolve()
             } else {
+                console.log("Order Cancelled");
                 reject()
             }
         }, 3000)
     })
 
     myOrder.then(function() {     // resolve -> run when order received
-        console.log("Now make a payment");
+        console.log("Making Payment");
 
         let paymentStatus = true
 
-        return new Promise(function(res) {
+        return new Promise(function(res, rej) {
 
         setTimeout(function() {
             if(paymentStatus) {
+                console.log("Payment Done...");
                 res()
             } else {
+                console.log("Payment Failed...");
                 rej()
             }
         }, 3000)
         })
-    })     
-    .then(function() {
-        console.log("Nacho");
+    }).then(function() {
+        console.log("I am eating food...");
+
+        let foodStatus = false
+        return new Promise(function(res, rej) {
+            setTimeout(function() {
+                if(foodStatus) {
+                    console.log("Pet bhar gya...");
+                    res()
+                } else {
+                    console.log("Pet nhi bhara...");
+                    rej()
+                }
+            }, 3000)
+        })
     })
     .catch(function() {    // reject -> run when order failed
-        console.log("Order failed");
-        console.log("Complain kro");
+        console.log("Order Cancelled...");
+    })
+    .finally(function() {
+        console.log("Process End");
     })
 }
 
 orderFood()
 
-// Async Promises
+// Handling Promises
+
+let p3 = fetch('https://fakestoreapi.com/products')
+
+p3
+  .then(function(data) {
+    console.log("Data aa gya");
+    return data.json()
+})
+  .then(function(myData) {
+    console.log(myData);
+})
+  .catch(function() {
+    console.log("Data nhi aaya");
+})
+
+
+async function dataLao() {
+    try {
+        let response = await fetch('https://fakestoreapi.com/products')
+
+        let data = await response.json()
+
+        data.forEach(function(elem) {
+            console.log(elem.title);  
+        })
+    } catch (error) {
+        console.error("Wrong URL");
+    }
+}
+dataLao()
