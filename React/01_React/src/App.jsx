4d26cee2193718e3890b07fe66627e4823612be2
Shashank Singh <sshingh456@gmail.com>
2026-06-27 16:55:21 +0530

    feat: initialize React Todo App with Vite and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created main application structure with App component
    - Implemented basic Todo functionality (add and delete)
    - Added Tailwind CSS for styling
    - Configured Vite for development and build processes
    - Included SVG assets for branding

---
+++ React/01_React/src/App.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [input, setInput] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const handleAdd = () => {
+    if(input.trim() === "") return
+
+    setTodos([...todos, input]) // add new todo
+    setInput("") // clear input
+  }
+
+  const handleDelete = (index) => {
+  const updated = todos.filter((item, i) => {
+    if (i !== index) {
+      return true   // keep it
+    } else {
+      return false  // remove it
+    }
+  })
+
+  setTodos(updated)
+}
+
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <div className='bg-blue-800 h-[500px] w-2/3 px-10'>
+
+        <h1 className='text-white text-3xl font-bold flex justify-center mt-3'>
+          Todo App
+        </h1>
+
+        <div className='bg-white mt-4 text-xl flex justify-between rounded-xl'>
+          
+          <input 
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text" 
+            placeholder='Add a todo'
+            className='px-2 py-2 flex-1 outline-none'
+          />
+
+          <button 
+            onClick={handleAdd}
+            className='bg-violet-700 py-2 px-3 rounded-xl text-white'
+          >
+            Add Todo
+          </button>
+
+        </div>
+
+        {/* Todo List */}
+        <div className='mt-6'>
+          {
+            todos.map((task, index) => (
+              <div key={index} className='text-white mt-2 flex justify-between'>
+                <h1 className='font-medium text-xl'>{task}</h1>
+                <button className='bg-red-600 px-3 py-1 rounded-xl' onClick={() => handleDelete(index)}>Delete</button>
+              </div>
+            ))
+          }
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
