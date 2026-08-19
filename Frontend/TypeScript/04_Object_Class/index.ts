f2b6ee82ae589ed1d67e9bf93cab93c626763f39
Shashank Singh <sshingh456@gmail.com>
2026-08-19 13:26:46 +0530

    Add ExpressJS backend with product API and TypeScript enhancements
    - Created package.json for ExpressJS project with dependencies for express and nodemon.
    - Implemented server.js to handle GET requests for products and a POST request for creating entries.
    - Added sample product data to the /products endpoint.
    - Enhanced TypeScript code with access modifiers, read-only properties, getters/setters, and static members in classes.

--- Frontend/TypeScript/04_Object_Class/index.ts
+++ Frontend/TypeScript/04_Object_Class/index.ts
@@ -17,3 +17,71 @@ class BottleMaker {
     }
 }
 let b1 = new BottleMaker("milton")
+
+
+// Access Modifiers
+class User {
+    private email: string = "email.com";
+    protected password: string = "1234";
+    constructor(public name: string) {}
+}
+
+class Admin extends User {
+    constructor(name: string) {
+        super(name);
+    }
+
+    getValue() {
+        console.log(this.name);
+        console.log(this.password);
+        // console.log(this.email);  //Error
+    }
+}
+
+let admin = new Admin("John");
+admin.getValue()
+
+// Read Only Properties
+
+class Car {
+    constructor(public readonly name: string) {}
+}
+let c1 = new Car("BMW");
+// c1.name = "Audi";
+console.log(c1.name);
+
+
+// Getter & Setter
+class Employee {
+    constructor(public name: string, public age: number) {}
+
+    get empName() {
+        return this.name
+    }
+
+    set empAge(value: number) {
+        this.age = value;
+    }
+
+    get empAge() {
+        return this.age
+    }
+}
+let emp1 = new Employee("raj", 20);
+console.log(emp1.empName)
+emp1.empAge = 10
+console.log(emp1.empAge)
+
+// Static Members -> 
+// You don’t need new to access them
+// You call them using the class name
+
+class Shery {
+    static version = 1.0;
+
+    static getRandomNumber() {
+        return Math.random()
+    }
+}
+
+console.log(Shery.getRandomNumber());
