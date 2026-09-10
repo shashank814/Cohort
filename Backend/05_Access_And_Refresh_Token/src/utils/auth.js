df538e4fa33f4c33e05c49f84fd5b419f9abdcbc
Shashank Singh <sshingh456@gmail.com>
2026-09-10 13:27:40 +0530

    feat: implement authentication routes and token generation/verification

---
+++ Backend/05_Access_And_Refresh_Token/src/utils/auth.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken"
+import config from "../config/config.js"
+
+export const generateToken = ({ userId }) => {
+    const accessToken = jwt.sign({
+        id: userId
+    }, config.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+    const refreshToken = jwt.sign({
+        id: userId
+    }, config.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
+
+    return { accessToken, refreshToken }
+}
+
+export function verifyAccessToken(token) {
+    
+    const decoded = jwt.verify(token, config.ACCESS_TOKEN_SECRET)
+
+    return decoded
+}
+
+export function verifyRefreshToken(token) {
+    
+    const decoded = jwt.verify(token, config.REFRESH_TOKEN_SECRET)
+
+    return decoded
+}
