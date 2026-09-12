63d035752f4e94fee7d7706ba77fc39f4f54ecf7
Shashank Singh <sshingh456@gmail.com>
2026-09-12 20:37:19 +0530

    fix: correct API endpoint URL in getUsers function

--- Frontend/React/17_Debouncing/src/apis/UsersApi.jsx
+++ Frontend/React/17_Debouncing/src/apis/UsersApi.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export let getUsers = async () => {
   console.log("api function running...");
-  let res = await axios.get("https://fakestoresapi.com/users");
+  let res = await axios.get("https://fakestoreapi.com/users");
   console.log(res.data);
 };
 
