62223e30f51bb77a8046e95fc36748ee54a6e8bd
Shashank Singh <sshingh456@gmail.com>
2026-09-19 16:21:52 +0530

    feat: add registration validation middleware for email, name, and password

---
+++ Backend/Practice Projects/05_Snitch/server/src/validators/auth.validator.js
@@ -0,0 +1,33 @@
+import { body, validationResult } from "express-validator";
+
+export const registerValidator = [
+  body("email")
+    .exists().withMessage("Email is required").bail()
+    .trim()
+    .isEmail().withMessage("Invalid email"),
+
+  body("name")
+    .exists().withMessage("Name number is required").bail()
+    .isString().withMessage("Name must be a string")
+    .trim()
+    .isLength({ min: 2, max: 50 }).withMessage("Name length must be between 2 to 50 characters"),
+
+  body("password")
+    .exists().withMessage("Password is required").bail()
+    .isString().withMessage("Password must be a string")
+    .trim()
+    .isLength({ min: 6 }).withMessage("Password at least 6 characters long"),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        message: "Invalid Request",
+        errors: errors.array(),
+      });
+    }
+
+    next();
+  },
+];
