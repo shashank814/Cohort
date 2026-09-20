6323b0a7e0d6940c78ea39640531c45d265eb719
Shashank Singh <sshingh456@gmail.com>
2026-09-20 16:38:28 +0530

    feat: update auth routes to include login and refresh endpoints; improve token expiration format in utils

--- Backend/Practice Projects/05_Snitch/server/src/routes/auth.routes.js
+++ Backend/Practice Projects/05_Snitch/server/src/routes/auth.routes.js
@@ -1,10 +1,14 @@
 import express, { Router } from "express"
-import { registerValidator } from "../validators/auth.validator.js"
-import { register } from "../controllers/auth.controller.js"
+import { loginValidator, registerValidator } from "../validators/auth.validator.js"
+import { getMe, login, refresh, register } from "../controllers/auth.controller.js"
+import { authneticate } from "../middleware/auth.middleware.js"
 
 const router = Router()
 
 router.post("/register", registerValidator, register)
+router.post("/login", loginValidator, login)
+router.post("/refresh", refresh)
 
+router.get("/me", authneticate, getMe)
 
 export default router
--- Backend/Practice Projects/05_Snitch/server/src/utils/auth.utils.js
+++ Backend/Practice Projects/05_Snitch/server/src/utils/auth.utils.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken"
 export function createAccessToken({ userId, role }) {
     const accessToken = jwt.sign({
         userId, role
-    }, config.ACCESS_TOKEN_SECRET, { expiresIn: "15MIN"})
+    }, config.ACCESS_TOKEN_SECRET, { expiresIn: "15m"})
 
     return accessToken
 }
@@ -13,8 +13,15 @@ export function createAccessToken({ userId, role }) {
 export function createRefreshToken({ userId, role }) {
     const refreshToken = jwt.sign({
         userId, role
-    }, config.REFRESH_TOKEN_SECRET, { expiresIn: "7dDays"})
+    }, config.REFRESH_TOKEN_SECRET, { expiresIn: "7d"})
 
     return refreshToken
 }
 
+export function readRefreshToken(refreshToken) {
+    return jwt.verify(refreshToken, config.REFRESH_TOKEN_SECRET)
+}
+
+export function readAccessToken(accessToken) {
+    return jwt.verify(accessToken, config.ACCESS_TOKEN_SECRET)
+}
