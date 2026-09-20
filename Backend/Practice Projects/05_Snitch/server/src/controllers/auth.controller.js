aeffda199fc6a2fb690c6166105495005e72108c
Shashank Singh <sshingh456@gmail.com>
2026-09-20 16:37:53 +0530

    feat: add cookie-parser dependency and implement authentication middleware

--- Backend/Practice Projects/05_Snitch/server/src/controllers/auth.controller.js
+++ Backend/Practice Projects/05_Snitch/server/src/controllers/auth.controller.js
@@ -1,36 +1,191 @@
 import userModel from "../models/user.model.js";
-import bcrypt from "bcryptjs"
-import { createAccessToken, createRefreshToken } from "../utils/auth.utils.js";
+import bcrypt from "bcryptjs";
+import { createAccessToken, createRefreshToken, readRefreshToken } from "../utils/auth.utils.js";
 
 export async function register(req, res) {
-    
-    const { name, email, password } = req.body
+  const { name, email, password } = req.body;
 
-    const isUserAlreadyExist = await userModel.findOne({ email })
+  const isUserAlreadyExist = await userModel.findOne({ email });
 
-    if(isUserAlreadyExist) {
+  if (isUserAlreadyExist) {
+    return res.status(400).json({
+      message: "User already exist with this email address",
+      errors: {
+        path: "email",
+        msg: "User already exist with this email address",
+      },
+    });
+  }
+
+  const user = await userModel.create({
+    name,
+    email,
+    password: await bcrypt.hash(password, 12),
+  });
+
+  const accessToken = createAccessToken({
+    userId: user._id,
+    role: user.role,
+  });
+  const refreshToken = createRefreshToken({
+    userId: user._id,
+    role: user.role,
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+  });
+
+  await userModel.findByIdAndUpdate(user._id, {
+    refreshToken: refreshToken
+  })
+
+  return res.status(200).json({
+    message: "User Registered Successfully",
+    data: {
+      email: user.email,
+      name: user.name,
+      id: user._id
+    },
+    accessToken
+  });
+}
+
+
+export async function login(req, res) {
+    const { email, password } = req.body
+
+    const user = await userModel.findOne({
+        email
+    })
+
+    if(!user) {
         return res.status(400).json({
-            message: "User already exist with this email address",
-            errors: {
-                field: "email",
-                message: "User already exist with this email address"
-            }
+            message: "Invalid email or password"
         })
     }
 
-    const user = await userModel.create({
-        name,
-        email,
-        password: await bcrypt.hash(password, 12)
-    })
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if(!isPasswordValid) {
+        return res.status(400).json({
+            message: "Invalid email or password"
+        })
+    }
 
     const accessToken = createAccessToken({
         userId: user._id,
         role: user.role
     })
+
     const refreshToken = createRefreshToken({
         userId: user._id,
         role: user.role
     })
 
+    await userModel.findOneAndUpdate({
+        email
+    }, {refreshToken})
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        message: "user loggedIn successfully",
+        data: {
+            user: {
+                email: user.email,
+                name: user.name,
+                id: user._id,
+            },
+            accessToken
+        }
+    })
+}
+
+
+export async function refresh(req, res) {
+    
+    
+    const refreshToken = req.cookies.refreshToken
+
+    if(!refreshToken) {
+        return res.status(401).json({
+            message: "Refresh token is required"
+        })
+    }
+
+    try {
+        
+        const decoded = readRefreshToken(refreshToken)
+
+        const { userId, role } = decoded
+
+        const user = await userModel.findById(userId)
+
+        if(refreshToken != user.refreshToken) {
+            await userModel.findByIdAndUpdate(user._id, {
+                refreshToken: null,
+                isAccountFreeze: true
+            })
+            return res.status(401).json({
+            message: "Refresh token mismatch"
+        })
+        }
+
+        const accessToken = createAccessToken({
+            userId, role
+        })
+
+        const newRefreshToken = createAccessToken({
+            userId, role
+        })
+
+        await userModel.findByIdAndUpdate(user._id, {
+            refreshToken: newRefreshToken
+        })
+
+        res.cookie("refreshToken", newRefreshToken, {
+            httpOnly: true
+        })
+
+        return res.status(200).json({
+            message: "Tokens rotated successfully",
+            data: {
+                user: {
+                    email: user.email,
+                    name: user.name,
+                    id: user._id
+                },
+                accessToken
+            }
+        })
+
+    } catch (error) {
+        
+        return res.status(401).json({
+            message: "Invalid refresh token"
+        })
+    }
+
+}
+
+
+export async function getMe(req, res) {
+
+    const { userId, role } = req.user
+    const user = await userModel.findById(userId)
+
+    res.status(200).json({
+        message: "user data fetched successfully",
+        data: {
+            user: {
+                email: user.email,
+                name: user.name,
+                id: user._id
+            }
+        }
+    })
+    
 }
---
+++ Backend/Practice Projects/05_Snitch/server/src/middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+import { readAccessToken } from "../utils/auth.utils.js";
+
+export function authneticate(req, res, next) {
+  const accessToken = req.headers.authorization?.split(" ")[1];
+
+  if (!accessToken) {
+    return res.status(400).json({
+      message: "Access token not found in the request header",
+    });
+  }
+
+  try {
+    const decoded = readAccessToken(accessToken);
+
+    req.user = decoded;
+
+    next()
+    
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid or expire access token",
+    });
+  }
+}
