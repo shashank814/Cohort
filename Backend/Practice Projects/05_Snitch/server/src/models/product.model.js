504c526c23784458c74ac7ac24d4f7f6e460731e
Shashank Singh <sshingh456@gmail.com>
2026-09-22 17:29:50 +0530

    feat: add product model and initial products route structure

---
+++ Backend/Practice Projects/05_Snitch/server/src/models/product.model.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 100,
+    },
+    description: {
+        type: String,
+        required: true,
+        minLength: 20,
+        maxLength: 500,
+    },
+    images: {
+        type: [{
+           type: String,
+        }],
+        validate: {
+            validator: images => images.length <= 5,
+            message: "A product can have at most 5 images"
+        }
+    },
+    price: {
+        amount: {
+            type: String,
+            required: true
+        },
+        currency: {
+            type: String,
+            enum: ["INR", "USD"],
+            default: "INR"
+        }
+    },
+    sizes: [
+        {
+            size: {
+                type: String,
+                enum: ["XS", "S", "M", "L", "XL", "XXL"],
+                required: true
+            },
+            stock: {
+                type: Number,
+                min: 0,
+                default: 0
+            }
+        }
+    ],
+    seller: {
+        user: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true
+    }
+})
+
+const productModel = mongoose.model("products", ProductSchema)
+export default productModel
---
+++ Backend/Practice Projects/05_Snitch/server/src/routes/products.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express"
+
+const router = Router()
+
+// router.post("/add-products", )
+
+export default router
